cd6bca29a8a90779850b447fad4275146ba3d883
Ahmet <kahramanza1597@hotmail.com>
2022-11-08 14:20:22 +0300

    Script
    Game starter added
    Camera movement fixes

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] GameObject objectToFollow; //camera gonna follow this shit object
-    
-    public Vector3 distance; //my own cinemachine go brrrr
+    [SerializeField] GameObject objectToLook;
+    public bool gameStarted = false;
+    double myTimer = 0;
+
+    public Vector3 distance = new Vector3(-4, -1.2f, 0); //my own cinemachine go brrrr
 
     private void Update() //unity calls this function once per frame
     {
         distance.z = objectToFollow.transform.position.z; //locking z axis
         transform.position = objectToFollow.transform.position - distance; //setting position of camera
+
     }
+    
 }
+//-4  -1.2
+//-5 , -5
---
+++ Assets/Scripts/GameStarter.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameStarter : MonoBehaviour
+{
+    CameraMovement cam;
+    Movement movement;
+    bool gameStarted = false;
+    bool coroutine = false;
+    float lerpValue = 0;
+
+    private void Start()
+    {
+        cam =Camera.main.GetComponent<CameraMovement>();
+        movement = GetComponent<Movement>();
+        movement.enabled = false;
+    }
+    private void Update()
+    {
+        if(!gameStarted && !coroutine)
+        {
+            if (Input.touchCount > 0)
+            {
+                coroutine = true;
+                StartCoroutine(LerpCamera());
+                movement.enabled = true;
+            }
+        }
+    }
+
+    IEnumerator LerpCamera()
+    {
+        cam.distance = Vector3.Lerp(cam.distance, new Vector3(-5, -5, 0), lerpValue);
+        yield return new WaitForSeconds(Time.deltaTime);
+        Camera.main.transform.LookAt(new Vector3(transform.position.x , transform.position.y ,0));
+        lerpValue += Time.deltaTime * 0.7f;
+        if (lerpValue < 1) StartCoroutine(LerpCamera());
+        else yield break;
+    }
+}
+
+
