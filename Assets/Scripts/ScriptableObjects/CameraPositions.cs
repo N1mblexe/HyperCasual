683cc3d7cdb932e78911a3df669b140e7303a0ba
Ahmet <kahramanza1597@hotmail.com>
2022-11-20 01:17:58 +0300

    Camera saving and lerping

---
+++ Assets/Scripts/ScriptableObjects/CameraPositions.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "camPos" , menuName = "CamPos")]
+public class CameraPositions : ScriptableObject
+{
+    public Vector3 pos1;
+    public Vector3 pos2;
+}
