cb9349740e6f04793c8a77605fa641328cfa1172
Ahmet <kahramanza1597@hotmail.com>
2022-11-27 18:01:41 +0300

    mission system

--- Assets/Scripts/UI_Scripts/MissionSystem.cs
+++ Assets/Scripts/UI_Scripts/MissionSystem.cs
@@ -9,7 +9,10 @@ public class MissionSystem : MonoBehaviour
     public static int cash = 0, diamond=0;
     private void Start()
     {
-        
+        PlayerPrefs.SetInt("maxCar", 350);
+        PlayerPrefs.SetInt("maxCarMissionPref", 0);
+        PlayerPrefs.SetInt("maxYearMissionPref", 0);
+        PlayerPrefs.SetInt("maxJumperMissionPref", 0);
     }
     private void Update()
     {
@@ -25,8 +28,41 @@ public class MissionSystem : MonoBehaviour
     }
     public void MaxCarMissionButton(int Cash)
     {
-        if (MaxCarMission(10))
+        if (MaxCarMission(10) && PlayerPrefs.GetInt("maxCarMissionPref") == 0)
         {
+            PlayerPrefs.SetInt("maxCarMissionPref", 1);
+            cash += Cash;
+        } 
+
+    }bool MaxYearMission(int CarNumber)
+    {
+        if (PlayerPrefs.GetInt("maxCar") >= CarNumber)
+        {
+            return true;
+        }
+        else return false;
+    }
+    public void MaxYearMissionButton(int Cash)
+    {
+        if (MaxYearMission(10) && PlayerPrefs.GetInt("maxYearMissionPref") == 0)
+        {
+            PlayerPrefs.SetInt("maxYearMissionPref", 1);
+            cash += Cash;
+        } 
+
+    }bool MaxJumperMission(int CarNumber)
+    {
+        if (PlayerPrefs.GetInt("maxCar") >= CarNumber)
+        {
+            return true;
+        }
+        else return false;
+    }
+    public void MaxJumperMissionButton(int Cash)
+    {
+        if (MaxJumperMission(10) && PlayerPrefs.GetInt("maxJumperMissionPref") == 0)
+        {
+            PlayerPrefs.SetInt("maxJumperMissionPref", 1);
             cash += Cash;
         } 
 
--- Assets/Scripts/UI_Scripts/UI_Shop.cs
+++ Assets/Scripts/UI_Scripts/UI_Shop.cs
@@ -29,16 +29,15 @@ public class UI_Shop : MonoBehaviour
     }
 
      void Start()
-    {
-
-      Item1.text = Items[0];
-      Item2.text = Items[1];
-      Item3.text = Items[2];
-      Item1P.text = prices[0].ToString();
-      Item2P.text = prices[1].ToString();
-      Item3P.text = prices[2].ToString();
-      Item1S.sprite = Sprites[0];
-      Item2S.sprite = Sprites[1];
-      Item3S.sprite = Sprites[2];
-    }
+     {
+        Item1.text = Items[0];
+        Item2.text = Items[1];
+        Item3.text = Items[2];
+        Item1P.text = prices[0].ToString();
+        Item2P.text = prices[1].ToString();
+        Item3P.text = prices[2].ToString();
+        Item1S.sprite = Sprites[0];
+        Item2S.sprite = Sprites[1];
+        Item3S.sprite = Sprites[2];
+     }
 }
